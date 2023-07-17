a513e417b095c15544e2c03aac20c103f96c8825
cae <eduardokae01@hotmail.com>
2023-07-17 09:41:57 -0300

    feat: implementa TDD

---
+++ src/exercicios.js
@@ -0,0 +1,72 @@
+// Exercicio 1
+function validateIsNumber(number) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 2
+function calculateTriangleArea(base, height) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 3
+function concatString(array) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 4
+function teamPoints(wins, ties) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 5
+function highestNumber(array) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 6
+function fizzBuzz(array) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 7
+function encoder(string) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 7
+function decoder(string) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 8
+function phoneNumber(array) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+// Exercicio 9
+function calculatePaint(heigh, width) {
+  // apague a linha abaixo para implementar código aqui
+  throw new Error('função não implementada');
+}
+
+module.exports = {
+  validateIsNumber,
+  calculateTriangleArea,
+  concatString,
+  teamPoints,
+  highestNumber,
+  fizzBuzz,
+  encoder,
+  decoder,
+  phoneNumber,
+  calculatePaint,
+};
---
+++ src/tests/mocks.js
@@ -0,0 +1,44 @@
+const arrayStrings1 = ['batata', 'cenoura', 'abacate', 'cebola', 'rúcula'];
+const arrayStrings2 = ['Joana', 'Amanda', 'Rebeca', 'Juliana', 'Letícia'];
+const arrayStrings3 = ['honda', 'bmw', 'kawasaki', 'apprillia', 'dafra'];
+
+const arrayNumbers1 = [1, 8, 3, 8, 4, 5, 7, 8, 1, 2, 2];
+const arrayNumbers2 = [3, 19, 13, 8, 4, 5, 17, 19];
+const arrayNumbers3 = [3, 71, 3, 18, 41, 5, 17, 8];
+const arrayNumbers4 = [3, 5, 15, 30, 9, 81];
+const arrayNumbers5 = [3, 15, 25, 24, 17, 9, 80];
+const arrayNumbers6 = [2, 5, 15, 31, 9, 50];
+const arrayNumbers7 = [1, 1, 9, 8, 8, 7, 3, 5, 5, 2, 6];
+const arrayNumbers8 = [4, 1, 9, 7, 8, 7, 3, 5, 5, 3, 4];
+const arrayNumbers9 = [4, 7, 9, 9, 8, 7, 3, 3, 5, 1, 1];
+const arrayNumbers10 = [4, 7, 1, 9, 8, 7, 3, 3, 5, 1, 1];
+
+const expectArray1 = ['fizz', 'buzz', 'fizzbuzz', 'fizzbuzz', 'fizz', 'fizz'];
+const expectArray2 = ['fizz', 'fizzbuzz', 'buzz', 'fizz', 'bug', 'fizz', 'buzz'];
+const expectArray3 = ['bug', 'buzz', 'fizzbuzz', 'bug', 'fizz', 'buzz'];
+
+const expectedPaintResponse1 = 'Você precisa de 1 lata de 5 litros, 4 latas de 1 litro e 1 lata de 0,5 litros para realizar esse trabalho.';
+const expectedPaintResponse2 = 'Você precisa de 1 lata de 5 litros e 1 lata de 0,5 litros para realizar esse trabalho.';
+const expectedPaintResponse3 = 'Você precisa de 4 latas de 1 litro e 1 lata de 0,5 litros para realizar esse trabalho.';
+
+module.exports = {
+  arrayStrings1,
+  arrayStrings2,
+  arrayStrings3,
+  arrayNumbers1,
+  arrayNumbers2,
+  arrayNumbers3,
+  arrayNumbers4,
+  arrayNumbers5,
+  arrayNumbers6,
+  arrayNumbers7,
+  arrayNumbers8,
+  arrayNumbers9,
+  arrayNumbers10,
+  expectArray1,
+  expectArray2,
+  expectArray3,
+  expectedPaintResponse1,
+  expectedPaintResponse2,
+  expectedPaintResponse3
+}
